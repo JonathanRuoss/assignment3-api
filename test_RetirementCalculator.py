fa5d7b019be01ac28f3271dc00a0d51be54022d8
Seryn13 <45214433+Seryn13@users.noreply.github.com>
2020-04-04 16:44:21 -0500

    Added test cases

--- test_RetirementCalculator.py
+++ test_RetirementCalculator.py
@@ -3,10 +3,16 @@ from RetirementCalculator import RetirementCalculator
 
 
 class RetirementCalculatorTestCase(unittest.TestCase):
-    def testRetirementCalc(self):
+    def testRetirementCalc1(self):
         calculator = RetirementCalculator(21, 70000, 20, 1000000)
         self.assertEqual(
             calculator.getFinalAge(), 74, "Should be 74"
+            )
+
+    def testRetirementCalc2(self):
+        calculator = RetirementCalculator(21, 70000, 20, 1000000)
+        self.assertEqual(
+            calculator.getGoalMet(), True, "Should be True"
             )
 
 
