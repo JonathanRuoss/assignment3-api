8c8fb7d6a10f863b470bd6b24d9126d0a7da8d62
Seryn13 <45214433+Seryn13@users.noreply.github.com>
2020-04-04 22:08:52 -0500

    Added unit tests

--- test_BmiCalculator.py
+++ test_BmiCalculator.py
@@ -3,10 +3,28 @@ from BmiCalculator import BmiCalculator
 
 
 class BmiCalculatorTestCase(unittest.TestCase):
-    def testBmiVal(self):
+    def testUnderweightBmiVal(self):
+        calculator = BmiCalculator(6, 0, 100)
+        self.assertEqual(
+            calculator.getBmiVal(), 13.9, "Should be 13.9"
+            )
+
+    def testNormalweightBmiVal(self):
         calculator = BmiCalculator(6, 0, 170)
         self.assertEqual(
             calculator.getBmiVal(), 23.6, "Should be 23.6"
+            )
+
+    def testOverweightBmiVal(self):
+        calculator = BmiCalculator(6, 0, 200)
+        self.assertEqual(
+            calculator.getBmiVal(), 27.8, "Should be 27.8"
+            )
+    
+    def testObeseBmiVal(self):
+        calculator = BmiCalculator(6, 0, 250)
+        self.assertEqual(
+            calculator.getBmiVal(), 34.7, "Should be 34.7"
             )
 
     def testUnderweightBmiCat(self):
