d309af47c3612d86d71abf46fafdef680bea7304
Seryn13 <45214433+Seryn13@users.noreply.github.com>
2020-04-04 21:46:07 -0500

    Added unit test

--- test_RetirementCalculator.py
+++ test_RetirementCalculator.py
@@ -9,13 +9,19 @@ class RetirementCalculatorTestCase(unittest.TestCase):
             calculator.getFinalAge(), 74, "Should be 74"
             )
 
-    def testRetirementCalc2(self):
+    def testRetirementCalc1(self):
+        calculator = RetirementCalculator(21, 70000, 20, 1000000000)
+        self.assertEqual(
+            calculator.getFinalAge(), 52932, "Should be 52932"
+            )
+
+    def testRetirementCalc3(self):
         calculator = RetirementCalculator(21, 70000, 20, 1000000)
         self.assertEqual(
             calculator.getGoalMet(), True, "Should be True"
             )
 
-    def testRetirementCalc3(self):
+    def testRetirementCalc4(self):
         calculator = RetirementCalculator(21, 70000, 20, 1000000000)
         self.assertEqual(
             calculator.getGoalMet(), False, "Should be False"
